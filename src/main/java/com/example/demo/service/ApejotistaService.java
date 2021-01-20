3b3edc9e7d7eab225de41e9d2fefd593ba659935
Milena <michailoffcomarella@gmail.com>
2021-01-20 15:17:07 -0300

    Separação de camadas feita e rodando.

--- src/main/java/com/example/demo/service/ApejotistaService.java
+++ src/main/java/com/example/demo/service/ApejotistaService.java
@@ -1,12 +1,7 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Apejotista;
 import com.example.demo.repository.ApejotistaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class ApejotistaService {
